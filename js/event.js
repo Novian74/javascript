006eb390ae30d80b5e0afc9947358e58013fe260
Novian74 <rahmandha74@gmail.com>
2023-01-04 20:53:35 +0700

    video 14

---
+++ js/event.js
@@ -0,0 +1,9 @@
+function tampil(b) {
+    a = document.querySelector("p").innerText="Belajar Event JS " + b;
+    // a.innerText="Belajar Event JS";
+    console.log("belajar event js");
+}
+
+judul.onclick= function () {
+    document.querySelector(".isi").innerHTML="belajar event js menggunakan id";
+}
