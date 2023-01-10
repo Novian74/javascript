a39167f0ad4b8ab28d0336a29d7aa73472b5f714
Novian74 <rahmandha74@gmail.com>
2023-01-10 11:51:04 +0700

    video 27

---
+++ js/json.js
@@ -0,0 +1,22 @@
+let tblmenu = [
+    {
+        "idmenu" : 1,
+        "idkategori": 1,
+        "menu": "Apel",
+        "harga": 3000,
+        "warna" : ["merah","kuning","hijau"],
+        "stok": true,
+        "keterangan":null
+    },
+    {
+        "idmenu" : 2,
+        "idkategori": 1,
+        "menu": "Pisang",
+        "harga": 4000,
+        "warna" : ["merah","kuning",],
+        "stok": false,
+        "keterangan":null
+    },
+];
+
+console.log(tblmenu[0].menu);
