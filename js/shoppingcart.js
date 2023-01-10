844dcf9b8c5130058126f4dbcad7793b3e2acbd7
Novian74 <rahmandha74@gmail.com>
2023-01-10 10:48:24 +0700

    video 23

--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -4,6 +4,7 @@ let tblmenu = [
     {idmenu:3,idkategori:2,menu:"Rendang",gambar:"rendang.jpg",harga:15000},
     {idmenu:4,idkategori:3,menu:"Es Teh",gambar:"esteh.jpg",harga:2000},
     {idmenu:5,idkategori:4,menu:"Es Buah",gambar:"esbuah.jpg",harga:7000},
+    {idmenu:6,idkategori:1,menu:"Pisang Sidoarjo",gambar:"pisang.jpg",harga:2000},
 ];
 
 let tampil = tblmenu.map(function (kolom) {
@@ -17,10 +18,24 @@ let tampil = tblmenu.map(function (kolom) {
     <div class="harga">
       <h2>${kolom.harga}</h2>
     </div>
+    <div class="btn-beli">
+        <button data-idmenu="${kolom.idmenu}">Beli</button>
+    </div>
   </div>`;
-})
+});
 
 let isi = document.querySelector(".produk");
 isi.innerHTML=tampil;
 
-console.log(tampil);
+let btnbeli = document.querySelectorAll(".btn-beli > button");
+
+let cart = [];
+
+for (let index = 0; index < btnbeli.length; index++) {
+    btnbeli[index].onclick = function () {
+        // console.log(btnbeli[index].dataset["idmenu"]);
+        cart.push(btnbeli[index].dataset["idmenu"]);
+    } 
+};
+
+console.log(cart);
