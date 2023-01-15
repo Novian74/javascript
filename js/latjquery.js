f52fb22fe7008ed7a21346d752bf7b831e9c888d
Novian74 <rahmandha74@gmail.com>
2023-01-15 10:16:34 +0700

    video 32

--- js/latjquery.js
+++ js/latjquery.js
@@ -4,4 +4,14 @@ $( document ).ready(function() {
 
 $(function () {
     $("#isi").html("<h1>Belajar Jquery</h1>");
+
+    $("#klik").click(function (e) { 
+        e.preventDefault();
+        alert("belajar javascript");
+    });
+
+    $("#isi").mouseleave(function () { 
+        alert("mouse leave");
+        console.log("mouse");
+    });
 })
